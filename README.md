df7e5790e5502ec5e4fc452b1e416138d7281d44
vpavlenko <cxielamiko@gmail.com>
2014-02-02 17:02:22 +0400

    init commit :kiss:

---
+++ README.md
@@ -0,0 +1,2 @@
+My personal front-end tools playground
+======================================
---
+++ js/disable_pointer_events.js
@@ -0,0 +1,13 @@
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
